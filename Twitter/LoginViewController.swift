93dfb808bfa022094521244340d8e1a41d490e3a
Chia-Lin Liu <chialin6@uw.edu>
2020-10-03 17:35:00 -0700

    stay log in/out status

--- Twitter/LoginViewController.swift
+++ Twitter/LoginViewController.swift
@@ -16,10 +16,19 @@ class LoginViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        // check whether logged in
+        if UserDefaults.standard.bool(forKey: "userLoggedIn") {
+            self.performSegue(withIdentifier: "loginToHome", sender: self)
+        }
+    }
+    
     @IBAction func onLoginButton(_ sender: Any) {
         let twitterRequestUrl = "https://api.twitter.com/oauth/request_token"
         TwitterAPICaller.client?.login(url: twitterRequestUrl, success: {
             // code for success
+            // set userLoggedIn to true once logged in
+            UserDefaults.standard.set(true, forKey: "userLoggedIn")
             self.performSegue(withIdentifier: "loginToHome", sender: self)
         }, failure: { (Error) in
             print("Log in failed!")
